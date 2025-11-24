cbad3062a87756cf866a544c8338eafccd686824
Rafael Aguiar <rafa.aguiar285@gmail.com>
2025-11-24 11:16:09 -0600

    Fix variable and import errors in main.py
    Corrected a variable name from tesla_data to gme_data when resetting the index. Fixed the import statement for tesla_revenue and gameStopRevenue to use the correct module name.

--- main.py
+++ main.py
@@ -48,12 +48,12 @@ gameStop = yf.Ticker('GME')
 #Extract stock information and save it in a dataframe
 gme_data = gameStop.history(period='max')
 #Reset the Index
-tesla_data.reset_index(inplace=True)
+gme_data.reset_index(inplace=True)
 #Display the five first rows
 print(tesla_data.head())
 
 
-from Webscraping_Extract_Revenue_Data.py import tesla_revenue, gameStopRevenue
+from Webscraping_Extract_Revenue_Data import tesla_revenue, gameStopRevenue
 
 #PLOT THE GRAPHS
 make_graph(tesla_data, tesla_revenue, 'Tesla')
